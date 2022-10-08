9e23aeda04b0e4485f647adb337271eec560fe66
Harrison Cramer <harrisoncramer@gmail.com>
2022-10-07 22:21:26 -0400

    Changed to methods (pg 53)

--- geometry/perimeter.go
+++ geometry/perimeter.go
@@ -1,8 +1,22 @@
 package geometry
 
+import "math"
+
+type Circle struct {
+	Radius float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+func (c Circle) Area() float64 {
+	return (c.Radius * c.Radius) * math.Pi
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
 }
 
 func Perimeter(r Rectangle) float64 {
--- geometry/perimeter_test.go
+++ geometry/perimeter_test.go
@@ -8,6 +8,12 @@ func AssertTestPassing(got float64, want float64, t *testing.T) {
 	}
 }
 
+func AssertTestPassingPrecise(got float64, want float64, t *testing.T) {
+	if got != want {
+		t.Errorf("got %g want %g", got, want)
+	}
+}
+
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{20.0, 5.0}
 	got := Perimeter(rectangle)
@@ -16,8 +22,19 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 3.0}
-	got := Area(rectangle)
-	want := 30.0
-	AssertTestPassing(got, want, t)
+	t.Run("Rectangles", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 3.0}
+		got := rectangle.Area()
+		want := 30.0
+		AssertTestPassing(got, want, t)
+	})
+
+	t.Run("Circles", func(t *testing.T) {
+		circle := Circle{
+			Radius: 3.0,
+		}
+		got := circle.Area()
+		want := 28.274333882308138
+		AssertTestPassingPrecise(got, want, t)
+	})
 }
