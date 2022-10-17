0925c41bed902d50ac1c37fd0fa3f9561eec0550
Harrison Cramer <harrisoncramer@gmail.com>
2022-10-16 21:52:22 -0400

    Finished defer and select (pg 129)

--- defer_and_select/defer_and_select.go
+++ defer_and_select/defer_and_select.go
@@ -1,21 +1,59 @@
 package deferandselect
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func Racer(url_a string, url_b string) string {
-	startA := time.Now()
-	http.Get(url_a)
-	aDuration := time.Since(startA)
-
-	startB := time.Now()
-	http.Get(url_b)
-	bDuration := time.Since(startB)
-
-	if aDuration > bDuration {
-		return url_b
+/*
+The "select" keyword lets you wait on multiple channels. The
+first one to send a value wins.
+*/
+func Racer(a, b string, timeout time.Duration) (winner string, e error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+		/* The time.After() function returns a chan. We can use
+		   it here to avoid blocking forever if our pings don't return */
+	case <-time.After(timeout * time.Millisecond):
+		return "", fmt.Errorf("Timed out waiting for %s and %s", a, b)
 	}
-	return url_a
 }
+
+/*
+Ping returns a channel immediately, but only closes
+it after the get call finishes inside the goroutine
+*/
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
+}
+
+/*
+This implementation hits each of the endpoints in
+sequence and then measures the response time of each
+of them to determine a winner
+*/
+// func measureResponseTime(url string) time.Duration {
+// 	start := time.Now()
+// 	http.Get(url)
+// 	return time.Since(start)
+// }
+//
+// func Racer(a, b string) (winner string) {
+//
+// 	aDuration := measureResponseTime(a)
+// 	bDuration := measureResponseTime(b)
+//
+// 	if aDuration < bDuration {
+// 		return a
+// 	}
+// 	return b
+// }
--- defer_and_select/defer_and_select_test.go
+++ defer_and_select/defer_and_select_test.go
@@ -7,25 +7,44 @@ import (
 	"time"
 )
 
+/* When testing servers we can wrap the handler in httptest.NewServer */
+func makeServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
+
 func TestRacer(t *testing.T) {
-	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(20 * time.Second)
-		w.WriteHeader(http.StatusOK)
-	}))
-	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	}))
+	t.Run("Should return the URL of the faster server", func(t *testing.T) {
+		slowServer := makeServer(20 * time.Millisecond)
+		fastServer := makeServer(0)
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		want := fastURL
+		got, _ := Racer(slowURL, fastURL, 10000)
 
-	slowServer.Close()
-	fastServer.Close()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("Returns an error on 10 second timeout", func(t *testing.T) {
+		superSlowServer := makeServer(30 * time.Millisecond)
+		otherSlowServer := makeServer(40 * time.Millisecond)
+		defer superSlowServer.Close()
+		defer otherSlowServer.Close()
+
+		slowURL := superSlowServer.URL
+		slowerURL := otherSlowServer.URL
+
+		_, err := Racer(slowURL, slowerURL, 10)
+		if err == nil {
+			t.Errorf("Expected timeout error but didn't get one")
+		}
+	})
 }
